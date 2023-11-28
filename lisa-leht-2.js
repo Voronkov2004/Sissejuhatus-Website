fa76a1f486415db36f75f647a030a7b10e4b328e
timur0108 <timur.sirazitdinov@gmail.com>
2023-11-28 17:21:32 +0200

    lisaleht1 ja lisaleht2

---
+++ lisa-leht-2.js
@@ -0,0 +1,8 @@
+function toggleSectionVisibility() {
+    var section = document.getElementById('additional-info');
+    if (section.style.display === 'none' || section.style.display === '') {
+        section.style.display = 'block';
+    } else {
+        section.style.display = 'none';
+    }
+}
