07c100c6e93e5c023530b6278ea2aa6f9fc00e4d
ralfkonr <150488593+ralfkonr@users.noreply.github.com>
2023-11-30 20:24:29 +0200

    Update lisa-leht-4.js

--- lisa-leht-4.js
+++ lisa-leht-4.js
@@ -1,1 +1,13 @@
-
+let image = document.getElementById('pilt1');
+function ZoomIn() {
+    let width = image.clientWidth;
+    let height = image.clientHeight;
+    image.style.width = (width + 50) + "px";
+    image.style.height = (height + 50) + "px";
+}
+function ZoomOut() {
+    let width = image.clientWidth;
+    let height = image.clientHeight;
+    image.style.width = (width - 50) + "px";
+    image.style.height = (height - 50) + "px";
+}
