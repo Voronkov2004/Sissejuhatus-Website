dd350f120a1e761867b4da20ac614f80f8d421b2
Anton <avoronkov2004@gmail.com>
2023-12-02 00:02:42 +0200

    upd

--- lisa-leht-1.js
+++ lisa-leht-1.js
@@ -1,8 +1,13 @@
+// Funktsioon toggleSectionVisibility
 function toggleSectionVisibility() {
-    var section = document.getElementById('additional-info');
-    if (section.style.display === 'none' || section.style.display === '') {
-        section.style.display = 'block';
-    } else {
-        section.style.display = 'none';
-    }
+    // Hangime elemendi ID-ga 'additional-info'
+   var section = document.getElementById('additional-info');
+   
+   // Kui elemendi stiil on 'none' või tühi (''), tee see nähtavaks ('block')
+   if (section.style.display === 'none' || section.style.display === '') {
+       section.style.display = 'block';
+   } else {
+       // Kui elemendi stiil ei ole 'none', tee see nähtamatuks ('none')
+       section.style.display = 'none';
+   }
 }
---
+++ lisa-leht-4.js
@@ -0,0 +1,13 @@
+let image = document.getElementById('pilt2');
+function ZoomIn() {
+    let width = image.clientWidth;
+    let height = image.clientHeight;
+    image.style.width = (width + 50) + "px";
+    image.style.height = (height + 50) + "px";
+}
+function ZoomOut() {
+    let width = image.clientWidth;
+    let height = image.clientHeight;
+    image.style.width = (width - 50) + "px";
+    image.style.height = (height - 50) + "px";
+}
