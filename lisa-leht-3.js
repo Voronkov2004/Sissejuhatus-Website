17ac2f337fcf71fd7023aa18982cc17479940bc8
ralfkonr <150488593+ralfkonr@users.noreply.github.com>
2023-12-04 20:49:00 +0200

    Update and rename lisa-leth-3.js to lisa-leht-3.js

--- lisa-leht-3.js
+++ lisa-leht-3.js
@@ -1,19 +1,19 @@
 let image = document.getElementById('pilt1');
-//sisse suumimine
+// sisse suumimine
 function ZoomIn() {
-    //saab pildi suuruse
+    // saab pildi suuruse
     let width = image.clientWidth;
     let height = image.clientHeight;
-    //muudab 50 piksli väärtuses
+    // muudab 50 piksli väärtuses
     image.style.width = (width + 50) + "px";
     image.style.height = (height + 50) + "px";
 }
-//välja suumimine
+// välja suumimine
 function ZoomOut() {
     //saab pildi suuruse
     let width = image.clientWidth;
     let height = image.clientHeight;
-    //muudab 50 piksli väärtuses
+    // muudab 50 piksli väärtuses
     image.style.width = (width - 50) + "px";
     image.style.height = (height - 50) + "px";
 }
